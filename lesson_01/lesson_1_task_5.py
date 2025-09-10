3ade6c9ac686daf0af63a9a3c98dfa8355700aec
Екатерина-тест2 <shnaider.ekater@yandex.ru>
2025-09-10 19:00:51 +0200

    lesson1

---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def print_param(num):
+    print(num, end='')
+
+
+print_param(8)
+print_param(8)
+print_param(0)
+print_param(0)
+print_param(5)
+print_param(5)
+print_param(5)
+print_param(3)
+print_param(5)
+print_param(3)
+print_param(5)
