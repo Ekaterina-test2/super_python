4e518a0f45ffc836af502a5caf91da57cb2f7a22
Екатерина-тест2 <shnaider.ekater@yandex.ru>
2025-11-12 15:35:25 +0200

    lesson8

---
+++ lesson_08/config.py
@@ -0,0 +1,11 @@
+base_url = 'https://ru.yougile.com/api-v2/'
+
+heade = {
+    "Content-Type": "application/json",
+    "Authorization": ""
+}
+
+payload = {
+    "title": "Моя тестовая организация",
+    "users": {}
+}
