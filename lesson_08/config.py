22d061fd731df50b354e5f735283211d2fb390f3
Екатерина-тест2 <shnaider.ekater@yandex.ru>
2025-11-13 08:36:05 +0200

    lesson8

--- lesson_08/config.py
+++ lesson_08/config.py
@@ -9,3 +9,9 @@ payload = {
     "title": "Моя тестовая организация",
     "users": {}
 }
+
+dann_auth = {
+  'login': '5cicbwd24r@zudpck.com',
+  'password': '12345Qwerty',
+  'companyId': 'ce00c8c0-53d4-4cc0-925d-6f0194d558c8'
+}
